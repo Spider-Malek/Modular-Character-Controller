3967dea4ff7171148becec63ae752848c15ad671
Spider-Malek <spidermalek666@gmail.com>
2023-03-12 18:22:17 +0200

    Init Commit
    The first commit

--- Assets/Scripts/Extensions/Builtin Types/BoolExtensions.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using UnityEngine;
-
-public static class BoolExtensions
-{ 
-	public static bool IsAllTrue (this IEnumerable<bool> bools) => bools.All(x => x == true);
-}
